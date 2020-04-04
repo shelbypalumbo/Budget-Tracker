7a2cb7bb78aa599c828593c19970f2426671205f
shelby palumbo <shelbypalumbo94@gmail.com>
2020-04-03 22:38:30 -0400

    Deleting unused code in service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,10 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "./src/index.js",
+  "./src/db.js",
   "/styles.css",
   "./dist/bundle.js",
-  "./dist/manifest.json",
-  "./src/db.js"
+  "./dist/manifest.08f84ee7cfb9720c93645e5bbaba4d22.json"
 ];
 
 const STATIC_CACHE = "static-cache-v1";
@@ -87,90 +87,3 @@ console.log(event.request)
     })
   );
 });
-
-
-
-
-
-
-
-
-
-
-
-// const CACHE_NAME = "static-cache-v6";
-// const DATA_CACHE_NAME = "data-cache-v3";
-
-// // install
-// self.addEventListener("install", function (evt) {
-//   evt.waitUntil(
-//     caches.open(CACHE_NAME).then(cache => {
-//       console.log("Your files were pre-cached successfully!");
-//       return cache.addAll(FILES_TO_CACHE);
-//     })
-//   );
-
-//   self.skipWaiting();
-// });
-
-// // activate
-// self.addEventListener("activate", function (evt) {
-//   evt.waitUntil(
-//     caches.keys().then(keyList => {
-//       return Promise.all(
-//         keyList.map(key => {
-//           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-//             console.log("Removing old cache data", key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
-
-//   self.clients.claim();
-// });
-
-// // fetch
-// // self.addEventListener("fetch", function(evt) {
-// //   console.log(evt.request);
-// //   if (evt.request.url.includes("/api/")) {
-// //     evt.respondWith(
-// //       caches.open(DATA_CACHE_NAME).then(cache => {
-// //         return fetch(evt.request)
-// //           .then(response => {
-// //             // If the response was good, clone it and store it in the cache.
-// //             if (response.status === 200) {
-// //               cache.put(evt.request.url, response.clone());
-// //             }
-
-// //             return response;
-// //           })
-// //           .catch(err => {
-// //             // Network request failed, try to get it from the cache.
-// //             return cache.match(evt.request);
-// //           });
-// //       }).catch(err => console.log(err))
-// //     );
-
-// //     return;
-// // }});
-// self.addEventListener("fetch", event => {
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then(cachedResponse => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
-
-//         return caches.open("runtime").then(cache => {
-//           return fetch(event.request).then(response => {
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       })
-//     );
-//   }
-// });
